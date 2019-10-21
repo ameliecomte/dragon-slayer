5b572e2fd5afab369651165974caf5778fd0cd44
ameliecomte <comamelie@gmail.com>
2019-10-21 15:55:43 +0200

    code js: declaration des variables, ecriture de fonctions 'utils' géneriques

---
+++ js/dragonslayer.js
@@ -0,0 +1,23 @@
+// 'use strict';   // Mode strict JavaScript
+
+/* ******************************************* UTILS ******************************************* */
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/* *************************************** DONNEES *************************************** */
+
+var game
+var damage = 0
+var round = 1
+const herotitle = ['Preux', 'Galant', 'Courageux', 'Hardi', 'Sage', 'Saint']
+const firstname = ['Javawan', 'Fafnir', 'Smaug', 'Drogon', 'Eliott', 'Alduin']
+const lastname = ['Maudis', 'Moche', 'Sinistre', 'Terrible', 'Malfaisant', 'Stupide']
+
+
+
+
+
+
+
